232935c4b273fd9c77506b228aba86f88ada3b20
xiaogang00 <872310734@qq.com>
2017-09-06 12:47:05 +0800

    c++ code for leetcode

---
+++ C++/flatten_binary_tree.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Theo on 2017/9/6.
+//
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+// Recursion
+class Solution {
+public:
+    void flatten(TreeNode *root) {
+        if (!root) return;
+        if (root->left) flatten(root->left);
+        if (root->right) flatten(root->right);
+        TreeNode *tmp = root->right;
+        root->right = root->left;
+        root->left = NULL;
+        while (root->right) root = root->right;
+        root->right = tmp;
+    }
+};
